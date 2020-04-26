46f55a9ec7513cdab5a82ad2f8fd0a5d5c193d00
Shkelqim Maxharraj <shkelqimimaxharraj@gmail.com>
2020-04-26 02:39:49 +0200

    Finishing the rerg'ex examples for subscribtion

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <meta name="description" content="Affordable and professional web design">
-	  <meta name="keywords" content="web design, affordable web design, professional web design">
-  	<meta name="author" content="Brad Traversy">
-    <title>Acme Web Deisgn | Welcome</title>
+    <meta name="description" content="Projekti ne programimi ne internet">
+	  <meta name="keywords" content="Projekt, Projekt shkollor, Projekti ne programi ne internet">
+  	<meta name="author" content="Shkelqimi">
+    <title>Programimi ne Internet</title>
     <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
@@ -20,7 +20,7 @@
             <li><a href="src/about.php">     About</a></li>
             <li><a href="src/services.php">  Services</a></li>
             <li><a href="src/Register.php">  Register</a></li>
-            <li><a href="src/Login.php"></a> LOGIN</li>
+            <li><a href="src/Login.php"></a> Login</li>
           </ul>
         </nav>
       </div>
@@ -28,33 +28,37 @@
 
     <section id="showcase">
       <div class="container">
-        <h1>Affordable Professional Web Design</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.</p>
+        <h1>Projekti: Programimi ne internet</h1>
+        <p> Teknologjite e perdorura ne projekt : Php, Html, Css, MySql, Linkedin Web Api </p>
       </div>
     </section>
 
     <section id="newsletter">
       <div class="container">
-        <h1>Subscribe To Our Newsletter</h1>
-        
-        <form action='index.php' method="POST" >
-          <input type="text" name='email' placeholder="Enter Email...">
-          <input type="submit" name='submit' class="button_1" value='Subscribe'>
-        </form>
-    
-      <?php 
-            if(isset($_POST['submit'])) if(empty($_POST['email'])){
-                echo 'An email is required to subscribe';
-            }
-            else{
-                $subemail= $_POST['email'];
+        <h1>Subscribe for other projects </h1>
 
-                if (!preg_match('/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/', $subemail))
-                {
-                    echo 'Please use the right email format to subscribe';
+          <?php
+            if(isset($_POST['submitSub'])){
+                if(empty($_POST['email'])){
+                    echo "Email can't be empty";
+                }
+                else {
+                    $email = $_POST['email'];
+                    if (!preg_match("/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/",$email)) {
+                        echo "Please use the right format for email";
+                    }
                 }
             }
-      ?>
+
+          ?>
+
+        <form name="subscribeForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+            <label for=""></label>
+            <input type="text" name='email' placeholder="Enter Email...">
+
+            <input type="submit" name='submitSub' class="button_1" value='Subscribe'>
+        </form>
+
 
       </div>
     </section>
@@ -62,17 +66,17 @@
     <section id="boxes">
       <div class="container">
         <div class="box">
-          <img src="./img/logo_html.jpg">
+          <img src="./img/logo_html.jpg" alt="Photo cannot be loaded...">
           <h3>TODO list</h3>
           <p>Manage your todo notes </p>
         </div>
         <div class="box">
-          <img src="./img/logo_css.png">
+          <img src="./img/logo_css.png" alt="Photo cannot be loaded...">
           <h3>Register</h3>
           <p>Register in our database</p>
         </div>
         <div class="box">
-          <img src="./img/logo_brush.png">
+          <img src="./img/logo_brush.png" alt="Photo cannot be loaded...">
           <h3>About me</h3>
           <p>Read about me to see the whole other requirments</p>
         </div>
